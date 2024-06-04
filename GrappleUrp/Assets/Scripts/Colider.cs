925df9581de77fe41bffc1fbaadd631b950afb94
Tobilinski <Tobiassherco125@gmail.com>
2024-06-04 14:56:09 +0200

    Started addeding interfaces to my project

--- GrappleUrp/Assets/Scripts/Colider.cs
+++ GrappleUrp/Assets/Scripts/Colider.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Colider : MonoBehaviour, IInteractable
 {
+    public void Expand()
+    {
+       this.transform.localScale = transform.localScale * 2;
+    }
+
     public void Interact()
     {
         print("Press E");
--- GrappleUrp/Assets/Scripts/Interactable.cs
+++ GrappleUrp/Assets/Scripts/Interactable.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public interface IInteractable
-{
-    void Interact();
-}
+
 public class Interactable : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -20,13 +17,6 @@ public class Interactable : MonoBehaviour
     {
         
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        IInteractable interactable = other.gameObject.GetComponent<IInteractable>();
-        if (interactable != null)
-        {
-            interactable.Interact();
-        }
-    }
+    
 }
 
